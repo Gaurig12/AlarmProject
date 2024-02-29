813487cabb61dc666590ad0f0838a5646ca83afa
Gaurig12 <gauri.gupta@bakerhughes.com>
2024-02-29 11:54:41 +0530

    fourth commit

---
+++ Ang/ui/src/app/app.model.ts
@@ -0,0 +1,17 @@
+// alarm.model.ts
+export interface Alarm {
+    id: number;
+    status: string;
+    type: string;
+    level: string;
+    project: string;
+    sprovider: string;
+    depth: string;
+    indepth: string;
+    systatus: string;
+    name : string;
+    well : string;
+    phase : string;
+    priority: string
+  }
+  
